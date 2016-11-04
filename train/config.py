14ecb3c38157153adab2758abc5582c19ad418ba
youhangtian <1013515028@qq.com>
2016-11-04 16:33:04 +0800

    Create config.py

---
+++ train/config.py
@@ -0,0 +1,18 @@
+#training config
+CAFFEPATH = 'path/to/caffe'
+
+SOLVER = 'path/to/solver.prototxt'
+TRAINED_FILE = 'path/to/trained.caffemodel'
+USE_GPU = 1
+DEVICE_ID = 1
+
+LMDB_PATH = 'path/to/train_lmdb'
+FOLDERS_PATH = 'path/to/train_folders'
+USE_LMDB = 0
+
+
+#triplet config
+BATCH_SIZE = 50
+CUT_SIZE = 5
+
+MARGIN = 0.2
