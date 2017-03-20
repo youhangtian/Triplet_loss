a48e05baccba74b3e83843094e188ec1767540e3
youhangtian <1013515028@qq.com>
2017-03-20 11:53:54 +0800

    Update config.py

--- train/config.py
+++ train/config.py
@@ -1,15 +1,14 @@
 #training config
-CAFFEPATH = 'path/to/caffe'
+CAFFEPATH = '/home/xiaotian/caffe'
 
-SOLVER = 'path/to/solver.prototxt'
-TRAINED_FILE = 'path/to/trained.caffemodel'
+SOLVER = '../models/inception21k_conv/inception21k_solver.prototxt'
+TRAINED_FILE = '/home/xiaotian/models/googlenet/Inception21k.caffemodel'
 USE_GPU = 1
 DEVICE_ID = 1
 
-LMDB_PATH = 'path/to/train_lmdb'
-FOLDERS_PATH = 'path/to/train_folders'
 USE_LMDB = 0
-
+LMDB_PATH = 'path/to/train_lmdb'
+DATA_FILE = 'filename'
 
 #triplet config
 BATCH_SIZE = 50
